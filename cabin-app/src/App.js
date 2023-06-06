5db33ec43d07e98f70b86ba8e5868e8a0f29a18d
JellonaVesa666 <vesaruntti@gmail.com>
2023-06-06 22:00:23 +0300

    Cleaning code.

--- cabin-app/src/App.js
+++ cabin-app/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef, useCallback } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import map from "./images/revised-campground-map-without-tents.png"
 import './App.css';
 
@@ -13,7 +13,7 @@ const usePrevious = (value) => {
 function App() {
 
   // Window size
-  const [screenSize, setScreenSize] = useState(getCurrentDimension());
+  const [windowSize, SetWindowSize] = useState(getCurrentDimension());
 
   //Marker Position
   const [markerPos, setMarkerPos] = useState({});
@@ -27,28 +27,36 @@ function App() {
   useEffect(() => {
 
     setCount(() => getCurrentDimension().width);
-    setMarkerPos({ x: Math.round((getCurrentDimension().width / prevCount) * markerPos.x), y: markerPos.y })
+    setMarkerPos({
+      x: Math.round(getCurrentDimension().width / prevCount * markerPos.x),
+      y: markerPos.y
+    })
 
     const updateDimension = () => {
-      setScreenSize(getCurrentDimension());
+      SetWindowSize(getCurrentDimension());
     }
     window.addEventListener('resize', updateDimension);
 
     return (() => {
       window.removeEventListener('resize', updateDimension);
     })
-  }, [screenSize])
+  }, [windowSize])
 
 
-  //console.log(mousePos.x / window.screen.width * screenSize.width);
+  //console.log(mousePos.x / window.screen.width * windowSize.width);
 
 
   const getMarkerPosition = (event) => {
     setMarkerPos({
-      x: Math.round(event.clientX / screenSize.width * screenSize.width),
-      y: Math.round(event.clientY / screenSize.height * screenSize.height)
+      x: CalculatePos(event.clientX, windowSize.width, windowSize.width),
+      y: CalculatePos(event.clientY, windowSize.height, windowSize.width),
     });
-  };
+  }
+
+
+  const CalculatePos = (A, B, C) => {
+    return Math.round(A / B * C)
+  }
 
 
   function getCurrentDimension() {
@@ -64,11 +72,11 @@ function App() {
         style={{ zIndex: -1, width: "100%", top: 0, left: 0, right: 0, bottom: 0, resizeMode: 'cover' }} />
       <div style={circle(markerPos)} />
       {/* <h1>{1 * multiplier.x}</h1> */}
-      <h1>{1 * screenSize.width}</h1>
+      <h1>{1 * windowSize.width}</h1>
       <h2>left: {1 * markerPos.x}</h2>
       <h2>top: {1 * markerPos.y}</h2>
-      <h2>leftFixed: {Math.round(1000 / window.screen.width * screenSize.width)}</h2>
-      <h2>topFixed: {Math.round(40 / window.screen.height * screenSize.height)}</h2>
+      <h2>leftFixed: {Math.round(1000 / window.screen.width * windowSize.width)}</h2>
+      <h2>topFixed: {Math.round(40 / window.screen.height * windowSize.height)}</h2>
     </div>
   );
 }
