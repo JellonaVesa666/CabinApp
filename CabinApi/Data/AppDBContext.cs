9a98900b50053c91be0a8c188909701c76dcc194
JellonaVesa666 <vesaruntti@gmail.com>
2023-06-07 20:53:33 +0300

    Update

---
+++ CabinApi/Data/AppDBContext.cs
@@ -0,0 +1,12 @@
+﻿using CabinApi.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CabinApi.Data
+{
+    public class AppDBContext: DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ CabinApi/Data/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CabinApi.Data.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int? Role { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string? Name { get; set; }
+    }
+}
