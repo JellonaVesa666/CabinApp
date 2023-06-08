ca959112cab369564f1fe7686b556029098f0c26
JellonaVesa666 <vesaruntti@gmail.com>
2023-06-08 14:45:20 +0300

    Update

--- cabin-app/src/App.js
+++ cabin-app/src/App.js
@@ -1,88 +1,55 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useCallback } from 'react';
 import map from "./images/revised-campground-map-without-tents.png"
+import cabins from "./test/testData.json"
 import './App.css';
 
-const usePrevious = (value) => {
-  const ref = useRef();
-  useEffect(() => {
-    ref.current = value;
-  });
-  return ref.current;
-};
-
 function App() {
+  const [markerPos, SetMarkerPos] = useState(0);
+  const [initialize, SetInitialize] = useState(false);
 
-  // Window size
-  const [windowSize, SetWindowSize] = useState({
-    width: window.innerWidth,
-    height: window.innerHeight
-  });
+  const imgRef = useCallback((node) => {
+    if (node !== null) {
+      if (!initialize) {
+        const promise = new Promise((resolve) =>
+          setTimeout(() => resolve(node.getBoundingClientRect())
+            , 500));
 
-  //Marker Position
-  const [markerPos, setMarkerPos] = useState({});
-
-  const imgRef = useRef();
-
-
-  const [count, setCount] = useState(0);
-  const prevCount = usePrevious(count);
-
-  useEffect(() => {
-
-    setCount({
-      width: window.innerWidth,
-      height: imgRef.current.height,
-    });
-
-    if (prevCount) {
-      console.log("true");
-      setMarkerPos({
-        x: Math.round(window.innerWidth / prevCount.width * markerPos.x),
-        y: Math.round(imgRef.current.height / prevCount.height * markerPos.y)
-      })
+        // Initialize windowSize
+        promise.then((result) => {
+          if (result) {
+            CalculateMarkerPos(node);
+            SetInitialize(true);
+          }
+        });
+      }
+      else {
+        // Update windowSize
+        const updateDimension = () => {
+          CalculateMarkerPos(node);
+        }
+        window.addEventListener('resize', updateDimension);
+      }
     }
+  }, [initialize]);
 
+  const CalculateMarkerPos = (node) => {
+    const boundingBox = node.getBoundingClientRect();
+    const X = ((boundingBox.right - boundingBox.left) * cabins.cabins[0].y) + boundingBox.left;
+    const Y = ((boundingBox.bottom - boundingBox.top) * cabins.cabins[0].x) + boundingBox.top;
 
-    const updateDimension = () => {
-      SetWindowSize({
-        width: window.innerWidth,
-        height: window.innerHeight
-      });
-    }
-    window.addEventListener('resize', updateDimension);
-
-    return (() => {
-      window.removeEventListener('resize', updateDimension);
+    SetMarkerPos({
+      x: X,
+      y: Y,
     })
-  }, [windowSize])
-
-  if (prevCount)
-    console.log(imgRef.current.height / prevCount.height * markerPos.y);
-
-
-  const getMarkerPosition = (event) => {
-    setMarkerPos({
-      x: CalculatePos(event.clientX, windowSize.width, windowSize.width),
-      y: CalculatePos(event.clientY, windowSize.height, windowSize.height),
-    });
-  }
-
-
-  const CalculatePos = (A, B, C) => {
-    return Math.round(A / B * C)
   }
 
   return (
     <div style={{ position: "relative" }}>
-      <img ref={imgRef} src={map} alt="" onClick={(event) => getMarkerPosition(event)}
-        style={{ zIndex: -1, width: "100%", top: 0, left: 0, right: 0, bottom: 0, resizeMode: 'cover' }} />
+      <div style={{ justifyContent: "center", display: "flex" }}>
+        <img ref={imgRef} src={map} alt=""
+          style={{ width: "70%", marginTop: "100px" }} />
+      </div>
       <div style={circle(markerPos)} />
-      {/* <h1>{1 * multiplier.x}</h1> */}
-      <h1>{1 * windowSize.width}</h1>
-      <h2>left: {1 * markerPos.x}</h2>
-      <h2>top: {1 * markerPos.y}</h2>
-      <h2>leftFixed: {Math.round(1000 / window.screen.width * windowSize.width)}</h2>
-      <h2>topFixed: {Math.round(40 / window.screen.height * windowSize.height)}</h2>
     </div>
   );
 }
@@ -94,7 +61,7 @@ const circle = (markerPos) => ({
   aspectRatio: 1 / 1,
   borderRadius: 22,
   background: "red",
-  left: 0,
+  left: markerPos.x,
   top: markerPos.y,
 });
 
