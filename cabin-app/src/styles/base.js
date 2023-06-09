e1f17dd82720ccc02d9126647b42bc339a57cf51
JellonaVesa666 <vesaruntti@gmail.com>
2023-06-09 14:21:47 +0300

    background image.

--- cabin-app/src/styles/base.js
+++ cabin-app/src/styles/base.js
@@ -1,7 +1,8 @@
 import styled from 'styled-components'
+import backgroundLight from "../images/background_Light.png";
 
 export const MainBody = styled.div`
-  background: red;
+  background-image: url(${backgroundLight});
   width: 100vw;
   height: 100vh;
 `
