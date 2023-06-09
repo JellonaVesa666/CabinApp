dfea3810aa3c1e4bef3e07b1546a649417e0cfe1
JellonaVesa666 <vesaruntti@gmail.com>
2023-06-09 14:06:05 +0300

    Update

---
+++ cabin-app/src/containers/LoginPage.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+//import "./Login.css";
+
+export default function Login() {
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
