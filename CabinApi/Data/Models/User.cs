51a0eeae9b7ff49418af4bd4c8eb00a26047b6a8
JellonaVesa666 <vesaruntti@gmail.com>
2023-06-11 21:01:19 +0300

    Update

--- CabinApi/Data/Models/User.cs
+++ CabinApi/Data/Models/User.cs
@@ -6,24 +6,20 @@ namespace CabinApi.Data.Models
     public class User
     {
         [Key]
-        [Required]
         public int Id { get; set; }
 
         public int? isActive { get; set; }
 
-        [Required]
         public int? Role { get; set; }
 
-        [Required]
+        [StringLength(8, ErrorMessage = "{0} length must be between {2} and {1}.", MinimumLength = 6)]
         [Column(TypeName = "nvarchar(50)")]
         public string? Name { get; set; }
 
-        [Required]
         [StringLength(8, ErrorMessage = "{0} length must be between {2} and {1}.", MinimumLength = 6)]
         [Column(TypeName = "nvarchar(50)")]
         public string? Email { get; set; }
 
-        [Required]
         [StringLength(8, ErrorMessage = "{0} length must be between {2} and {1}.", MinimumLength = 6)]
         [Column(TypeName = "nvarchar(50)")]
         public string? Password { get; set; }
