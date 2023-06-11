4291e519b9b70fc3d9c21295a1b47bdf29620b59
JellonaVesa666 <vesaruntti@gmail.com>
2023-06-11 20:19:01 +0300

    User model update

--- CabinApi/Data/Models/User.cs
+++ CabinApi/Data/Models/User.cs
@@ -8,9 +8,17 @@ namespace CabinApi.Data.Models
         [Key]
         public int Id { get; set; }
 
+        public int? isActive { get; set; }
+
         public int? Role { get; set; }
 
         [Column(TypeName = "nvarchar(50)")]
         public string? Name { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string? Email { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string? Password { get; set; }
     }
 }
