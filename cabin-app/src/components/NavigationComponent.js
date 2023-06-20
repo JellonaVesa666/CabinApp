cd0714e976a5b8d30912c7a61937cdca6694615b
JellonaVesa666 <vesaruntti@gmail.com>
2023-06-20 18:35:43 +0300

    Update

--- cabin-app/src/components/NavigationComponent.js
+++ cabin-app/src/components/NavigationComponent.js
@@ -10,26 +10,25 @@ export default function NavigationComponent() {
   return (
     <>
       <NavigationBody>
-          <NavigationContainer className={hideExtended ? "shadow-lg hide" : "shadow-lg show"}>
-            <Navigation className={hideExtended ? "hide" : "show"}>
-              <div style={{ height: "10%", width: "100%", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
-                <NavigationLink className="user" />
-              </div>
-              <div style={{ height: "80%", width: "100%", display: "flex", flexDirection: "column", alignItems:"center"}}>
-                <NavigationLink className="reservations" onClick={() => setHideExtend(!hideExtended)} />
-                <NavigationLink className="users" />
-                <NavigationLink className="cabins" />
-                <NavigationLink className="map" />
-                <NavigationLink className="settings" />
-              </div>
-              <div style={{ height: "10%", width: "100%", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
-                <NavigationLink className="logout" />
-              </div>
-            </Navigation>
-          </NavigationContainer>
+        <NavigationContainer className={hideExtended ? "shadow-lg hide" : "shadow-lg show"}>
+          <Navigation className={hideExtended ? "hide" : "show"}>
+            <div style={{ height: "10%", width: "100%", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
+              <NavigationLink className="user" />
+            </div>
+            <div style={{ height: "80%", width: "100%", display: "flex", flexDirection: "column", alignItems: "center" }}>
+              <NavigationLink className="reservations" onClick={() => setHideExtend(!hideExtended)} />
+              <NavigationLink className="users" />
+              <NavigationLink className="cabins" />
+              <NavigationLink className="map" />
+              <NavigationLink className="settings" />
+            </div>
+            <div style={{ height: "10%", width: "100%", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
+              <NavigationLink className="logout" />
+            </div>
+            <div style={{ position: "absolute", bottom: 0, left: "25%", backgroundColor: "rgba(25, 25, 25, 0.4)", height: "100%", width: "75%" }}></div>
+          </Navigation>
+        </NavigationContainer>
       </NavigationBody>
     </>
   )
 }
-
-/* className={data.fullName.errors.length > 0 ? "invalid" : ""} */
