acc42a4dc492ba10f106df92d1a6766a1e51f16a
JellonaVesa666 <vesaruntti@gmail.com>
2023-07-10 06:43:46 +0300

    update

--- cabin-app/src/components/DatePickerComponent.js
+++ cabin-app/src/components/DatePickerComponent.js
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { Calendar, MonthPanel, WeekGrid, Days, DayGrid } from "../styles/InputStyle";
+import { dayNames, monthNames, reservations } from "../mockup/calendarData";
 
 export const DatePicker = () => {
   const [currentDays, setCurrentDays] = useState();
@@ -26,98 +27,7 @@ export const DatePicker = () => {
     }
   }
 
-  useEffect(() => {
-    setCurrentDays(Range());
-  }, [currentMonth]);
-
-  const monthNames = {
-    1: "january",
-    2: "february",
-    3: "march",
-    4: "april",
-    5: "may",
-    6: "june",
-    7: "july",
-    8: "august",
-    9: "september",
-    10: "october",
-    11: "november",
-    12: "december"
-  }
-
-  const dayNames = [
-    "mon",
-    "tue",
-    "wed",
-    "thu",
-    "fri",
-    "sat",
-    "sun"
-  ]
-
-  const reservations = {
-    2023: {
-      april: {
-        0: {
-          arrivalDate: 1,
-          departureDate: 8,
-          name: ""
-        }
-      },
-      may: {
-        0: {
-          arrivalDate: 8,
-          departureDate: 15,
-          name: ""
-        }
-      },
-      june: {
-        0: {
-          arrivalDate: 1,
-          departureDate: 3,
-          name: ""
-        },
-        1: {
-          arrivalDate: 6,
-          departureDate: 8,
-          name: ""
-        }
-      },
-      july: {
-        0: {
-          arrivalDate: 8,
-          departureDate: 15,
-          name: ""
-        }
-      },
-      august: {
-        0: {
-          arrivalDate: 8,
-          departureDate: 15,
-          name: ""
-        }
-      },
-      september: {
-        0: {
-          arrivalDate: 8,
-          departureDate: 15,
-          name: ""
-        }
-      }
-    },
-    2022: {
-      september: {
-        0: {
-          arrivalDate: 8,
-          departureDate: 15,
-          name: ""
-        }
-      }
-    }
-  }
-
-
-  const Range = () => {
+  const range = useCallback(() => {
     const startDay = dayNames.indexOf((new Date(currentYear, currentMonth, 1)).toString().toLowerCase().split(' ')[0]);
     const endDay = dayNames.indexOf((new Date(currentYear, currentMonth + 1, 0)).toString().toLowerCase().split(' ')[0]);
 
@@ -125,20 +35,20 @@ export const DatePicker = () => {
     var prevMonth = [];
     const previousMonthLenght = new Date(currentYear, currentMonth, 0).getDate();
     for (var p = previousMonthLenght - startDay + 1; p <= previousMonthLenght; p++) {
-      let item = { "day": p, "month": "false", "reserved": "false" }
+      let item = { "day": p, "month": "false", "reserved": "false", "active": false }
       prevMonth.push(item);
 
       // Set Date Selected
       let month = monthNames[currentMonth];
       let index = p - (previousMonthLenght - startDay + 1);
-      if (reservations[currentYear] && reservations[currentYear][month]) {
-        Object.keys(reservations[currentYear][month]).forEach((element) => {
-          if (prevMonth[index].day >= reservations[currentYear][month][element].arrivalDate &&
-            prevMonth[index].day <= reservations[currentYear][month][element].departureDate) {
-            prevMonth[index].reserved = "true"
-          }
-        });
-      }
+      /*       if (reservations[currentYear] && reservations[currentYear][month]) {
+              Object.keys(reservations[currentYear][month]).forEach((element) => {
+                if (prevMonth[index].day >= reservations[currentYear][month][element].arrivalDate &&
+                  prevMonth[index].day <= reservations[currentYear][month][element].departureDate) {
+                  prevMonth[index].reserved = "true"
+                }
+              });
+            } */
     }
     //console.log(prevMonth);
 
@@ -146,20 +56,20 @@ export const DatePicker = () => {
     if (endDay < 6) {
       var nextMonth = [];
       for (var n = endDay; n < 6; n++) {
-        let item = { "day": n - endDay + 1, "month": "false", "reserved": "false" }
+        let item = { "day": n - endDay + 1, "month": "false", "reserved": "false", "active": false }
         nextMonth.push(item);
 
         // Set Date Selected
         let month = monthNames[currentMonth + 2];
         let index = n - endDay + 1 - 1;
-        if (reservations[currentYear] && reservations[currentYear][month]) {
-          Object.keys(reservations[currentYear][month]).forEach((element) => {
-            if (nextMonth[index].day >= reservations[currentYear][month][element].arrivalDate &&
-              nextMonth[index].day <= reservations[currentYear][month][element].departureDate) {
-              nextMonth[index].reserved = "true"
-            }
-          });
-        }
+        /*         if (reservations[currentYear] && reservations[currentYear][month]) {
+                  Object.keys(reservations[currentYear][month]).forEach((element) => {
+                    if (nextMonth[index].day >= reservations[currentYear][month][element].arrivalDate &&
+                      nextMonth[index].day <= reservations[currentYear][month][element].departureDate) {
+                      nextMonth[index].reserved = "true"
+                    }
+                  });
+                } */
       }
     }
     //console.log(nextMonth);
@@ -168,7 +78,7 @@ export const DatePicker = () => {
     const thisMonthLenght = new Date(currentYear, currentMonth + 1, 0).getDate();
     var thisMonth = [];
     for (var t = 1; t <= thisMonthLenght; t++) {
-      let item = { "day": t, "month": "this", "reserved": "false" }
+      let item = { "day": t, "month": "this", "reserved": "false", "active": false }
       thisMonth.push(item);
 
       // Set Date Selected
@@ -198,40 +108,44 @@ export const DatePicker = () => {
       var offset = Object.keys(monthDays).length > 28 ? 1 : 8;
       var extraWeeks = [];
       for (var e = 1; e <= endVal; e++) {
-        let item = { "day": (endVal - offset) - endDay + e, "month": "false", "reserved": "false" }
+        let item = { "day": (endVal - offset) - endDay + e, "month": "false", "reserved": "false", "active": false }
         extraWeeks.push(item);
 
         // Set Date Selected
-        let month = monthNames[currentMonth + 2];
-        let index = e - 1;
-        if (reservations[currentYear] && reservations[currentYear][month]) {
-          Object.keys(reservations[currentYear][month]).forEach((element) => {
-            if (extraWeeks[index].day >= reservations[currentYear][month][element].arrivalDate &&
-              extraWeeks[index].day <= reservations[currentYear][month][element].departureDate) {
-              extraWeeks[index].reserved = "true"
-            }
-          });
-        }
+        /*         let month = monthNames[currentMonth + 2];
+                let index = e - 1;
+                if (reservations[currentYear] && reservations[currentYear][month]) {
+                  Object.keys(reservations[currentYear][month]).forEach((element) => {
+                    if (extraWeeks[index].day >= reservations[currentYear][month][element].arrivalDate &&
+                      extraWeeks[index].day <= reservations[currentYear][month][element].departureDate) {
+                      extraWeeks[index].reserved = "true"
+                    }
+                  });
+                } */
       }
       monthDays = monthDays.concat(extraWeeks);
     }
     //console.log(extraWeeks);
 
     return monthDays;
-  };
+  }, [currentMonth, currentYear]);
+
+  useEffect(() => {
+    setCurrentDays(range());
+  }, [range]);
 
   const Test = (day, month) => {
-    console.log(day + "    " + month);
     currentDays.forEach((item, index) => {
-
       if (item.day === day && item.month === month) {
-        console.log(currentDays[index]);
-        /*         const newIngredients = [...currentDays];
-                newIngredients[index] = {
-                  ...newIngredients[index],
-                  reserved: 'true'
-                };
-                setCurrentDays(newIngredients); */
+        if (currentDays[index].reserved === "false") {
+          console.log(currentDays[index]);
+          const newIngredients = [...currentDays];
+          newIngredients[index] = {
+            ...newIngredients[index],
+            active: !newIngredients[index].active
+          };
+          setCurrentDays(newIngredients);
+        }
       }
     });
   }
@@ -240,14 +154,14 @@ export const DatePicker = () => {
     <Calendar>
       <MonthPanel>
         <p
-          style={{ fontSize: "30px", color: "white", marginTop: "auto", marginBottom: "auto", cursor: "pointer" }}
+          style={{ fontSize: "30px", marginTop: "auto", marginBottom: "auto", cursor: "pointer" }}
           onClick={() => prevMonth()}
         >
           &#9664;
         </p>
-        <p style={{ fontSize: "30px", color: "white", margin: "auto" }}>{monthNames[currentMonth + 1]} / {currentYear}</p>
+        <p style={{ fontSize: "30px", color: "black", margin: "auto" }}>{monthNames[currentMonth + 1].toUpperCase()} / {currentYear}</p>
         <p
-          style={{ fontSize: "30px", color: "white", marginTop: "auto", marginBottom: "auto", cursor: "pointer" }}
+          style={{ fontSize: "30px", marginTop: "auto", marginBottom: "auto", cursor: "pointer" }}
           onClick={() => nextMonth()}
         >
           &#9654;
@@ -255,7 +169,7 @@ export const DatePicker = () => {
       </MonthPanel>
       <WeekGrid>
         {dayNames.map((item, index) =>
-          <p style={{ color: "white" }} key={index}>{item}</p>
+          <p style={{ color: "black" }} key={index}>{item.toUpperCase()}</p>
         )}
       </WeekGrid>
       <DayGrid>
@@ -263,7 +177,7 @@ export const DatePicker = () => {
           return (
             <Days
               key={index}
-              className={`${item.month} ${item.reserved}`}
+              className={`${item.month} ${item.reserved ? "reserved" : ""} ${item.active ? "active" : ""}`}
               onClick={() => Test(item.day, item.month)}
             >
               {item.day}
