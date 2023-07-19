f2f4244498fd156673fef7802e44bbc890dfebf5
JellonaVesa666 <vesaruntti@gmail.com>
2023-07-19 15:02:52 +0300

    Update

--- cabin-app/src/components/SidebarModule.js
+++ cabin-app/src/components/SidebarModule.js
@@ -11,102 +11,98 @@ export const SidebarModule = ({ isActive }) => {
   const language = useSelector(state => state.session.language);
 
   const [modalActive, setModalActive] = useState(false);
-  const [dropdownActive, setdropdownActive] = useState(true);
   const [searchFilters, setSearchFilters] = useState(searchParameters);
 
   const listItems = Object.keys(searchFilters).map(item => {
     if (searchFilters[item].isActive)
       return (
-        <>
-          {dropdownActive &&
-            <FilterList
-              key={Object.keys(searchFilters).indexOf(item)}
-              className={searchFilters[item].dropdown ? "dropdownActive h-auto col-12 row m-0 p-0" : "h-auto col-12 row m-0 p-0"}
+
+        <FilterList
+          key={Object.keys(searchFilters).indexOf(item)}
+          className={searchFilters[item].dropdown ? "dropdownActive h-auto col-12 row m-0 p-0" : "h-auto col-12 row m-0 p-0"}
+        >
+          <FilterCard className="col-12 row mx-0 p-0">
+            <div className="col-1 py-2 " onClick={() => ChangeState(setSearchFilters, searchFilters, false, "isActive", item)}>
+              -
+            </div>
+            <CardLabel className=" py-2 col-6">
+              {ValidateElement(searchFilters[item]?.info?.[language]?.name, "label")}
+            </CardLabel>
+            <div
+              className="d-flex col-5 py-2 justify-content-end"
+              onClick={() => ChangeState(setSearchFilters, searchFilters, !searchFilters[item].dropdown, "dropdown", item)}
             >
-              <FilterCard className="col-12 row mx-0 p-0">
-                <div className="col-1 py-2 " onClick={() => ChangeState(setSearchFilters, searchFilters, false, "isActive", item)}>
-                  -
-                </div>
-                <CardLabel className=" py-2 col-6">
-                  {ValidateElement(searchFilters[item]?.info?.[language]?.name, "label")}
-                </CardLabel>
-                <div
-                  className="d-flex col-5 py-2 justify-content-end"
-                  onClick={() => ChangeState(setSearchFilters, searchFilters, !searchFilters[item].dropdown, "dropdown", item)}
-                >
-                  {searchFilters[item].dropdown ? <>&#9650;</> : <>&#x25BC;</>}
-                </div>
-                {
-                  searchFilters[item].dropdown &&
+              {searchFilters[item].dropdown ? <>&#9650;</> : <>&#x25BC;</>}
+            </div>
+            {
+              searchFilters[item].dropdown &&
+              <>
+                {searchFilters[item].type === "multiSelect" &&
+                  <MultiSelect
+                    width={"75%"}
+                    radius={"6px"}
+                    data={searchFilters}
+                    i={item}
+                    changeState={(value, index) => ChangeState(setSearchFilters, searchFilters, !value, "value", item, index)}
+                  />
+                }
+                {searchFilters[item].type === "option" &&
                   <>
-                    {searchFilters[item].type === "multiSelect" &&
-                      <MultiSelect
-                        width={"75%"}
-                        radius={"6px"}
-                        data={searchFilters}
-                        i={item}
-                        changeState={(value, index) => ChangeState(setSearchFilters, searchFilters, !value, "value", item, index)}
-                      />
-                    }
-                    {searchFilters[item].type === "option" &&
-                      <>
-                        <OptionSelect
-                          width={"75%"}
-                          radius={"6px"}
-                          data={searchFilters}
-                          i={item}
-                          changeState={(event) => ChangeState(setSearchFilters, searchFilters, event.target.value, "value", item)}
-                        />
-                      </>
-                    }
-                    {searchFilters[item].type === "counter" &&
-                      <Counter
-                        data={searchFilters}
-                        i={item}
-                        changeState={(value, index) => ChangeState(setSearchFilters, searchFilters, value, "value", item, index)}
-                      />
-                    }
-                    {searchFilters[item].type === "slider" &&
-                      <RangeSlider
-                        minDefault={searchFilters[item].minDefault}
-                        maxDefault={searchFilters[item].maxDefault}
-                        maxValue={searchFilters[item].maxValue}
-                        minValue={searchFilters[item].minValue}
-                        step={searchFilters[item].step}
-                        changeState={(value, property) => ChangeState(setSearchFilters, searchFilters, value, property, item)}
-                      />
-                    }
-                    {(() => {
-                      if (searchFilters[item].type.split(/(?=[A-Z])/)[0] === "checkbox") {
-                        if (searchFilters[item].type.split(/(?=[A-Z])/)[1] === "Multi") {
-                          return (
-                            <CheckBox
-                              data={searchFilters}
-                              i={item}
-                              multi={true}
-                              color={colors.green}
-                              changeState={(value, index) => ChangeState(setSearchFilters, searchFilters, !value, "value", item, index)}
-                            />
-                          )
-                        } else if (searchFilters[item].type.split(/(?=[A-Z])/)[1] === "Single") {
-                          return (
-                            <CheckBox
-                              data={searchFilters}
-                              i={item}
-                              single={true}
-                              color={colors.green}
-                              changeState={(index) => ChangeState(setSearchFilters, searchFilters, index, "value", item)}
-                            />
-                          )
-                        }
-                      }
-                    })()}
+                    <OptionSelect
+                      width={"75%"}
+                      radius={"6px"}
+                      data={searchFilters}
+                      i={item}
+                      changeState={(event) => ChangeState(setSearchFilters, searchFilters, event.target.value, "value", item)}
+                    />
                   </>
                 }
-              </FilterCard>
-            </FilterList>
-          }
-        </>
+                {searchFilters[item].type === "counter" &&
+                  <Counter
+                    data={searchFilters}
+                    i={item}
+                    changeState={(value, index) => ChangeState(setSearchFilters, searchFilters, value, "value", item, index)}
+                  />
+                }
+                {searchFilters[item].type === "slider" &&
+                  <RangeSlider
+                    minDefault={searchFilters[item].minDefault}
+                    maxDefault={searchFilters[item].maxDefault}
+                    maxValue={searchFilters[item].maxValue}
+                    minValue={searchFilters[item].minValue}
+                    step={searchFilters[item].step}
+                    changeState={(value, property) => ChangeState(setSearchFilters, searchFilters, value, property, item)}
+                  />
+                }
+                {(() => {
+                  if (searchFilters[item].type.split(/(?=[A-Z])/)[0] === "checkbox") {
+                    if (searchFilters[item].type.split(/(?=[A-Z])/)[1] === "Multi") {
+                      return (
+                        <CheckBox
+                          data={searchFilters}
+                          i={item}
+                          multi={true}
+                          color={colors.green}
+                          changeState={(value, index) => ChangeState(setSearchFilters, searchFilters, !value, "value", item, index)}
+                        />
+                      )
+                    } else if (searchFilters[item].type.split(/(?=[A-Z])/)[1] === "Single") {
+                      return (
+                        <CheckBox
+                          data={searchFilters}
+                          i={item}
+                          single={true}
+                          color={colors.green}
+                          changeState={(index) => ChangeState(setSearchFilters, searchFilters, index, "value", item)}
+                        />
+                      )
+                    }
+                  }
+                })()}
+              </>
+            }
+          </FilterCard>
+        </FilterList>
       )
   })
 
@@ -179,6 +175,7 @@ export const SidebarModule = ({ isActive }) => {
                 {persons}
               </div>
             </div>
+            {listItems}
             <div
               className="d-flex mb-4 mt-5 justify-content-center align-items-center row w-100"
               style={{ height: "40px" }}
@@ -189,7 +186,7 @@ export const SidebarModule = ({ isActive }) => {
                 className="d-flex justify-content-center align-items-center text-uppercase"
                 style={{ color: colors.black, width: "180px", height: "40px", marginLeft: "45px", borderRadius: " 10px 0px 0px 10px" }}
               />
-              <div className="d-flex justify-content-center align-items-center" onClick={() => setdropdownActive(!dropdownActive)} style={{ height: "40px", width: "40px", backgroundColor: "black", color: "white", fontSize: "12px", borderRadius: " 0px 10px 10px 0px" }}>
+              <div className="d-flex justify-content-center align-items-center" onClick={() => setModalActive(!modalActive)} style={{ height: "40px", width: "40px", backgroundColor: "black", color: "white", fontSize: "12px", borderRadius: " 0px 10px 10px 0px" }}>
                 &equiv;
               </div>
             </div>
@@ -197,7 +194,6 @@ export const SidebarModule = ({ isActive }) => {
               className="d-flex justify-content-start align-items-center flex-column w-100"
               style={{ height: "58%" }}
             >
-              {/* {listItems} */}
             </div>
           </SidebarCollapsed>
         </Sidebar>
