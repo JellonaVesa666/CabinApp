63155f0c305988738bddad0e5cc0c086288609a6
JellonaVesa666 <vesaruntti@gmail.com>
2023-07-25 22:17:39 +0300

    Update

--- cabin-app/src/components/CalendarModule.js
+++ cabin-app/src/components/CalendarModule.js
@@ -38,7 +38,7 @@ export const CalendarModule = (props) => {
     var prevMonth = {};
     const previousMonthLenght = new Date(currentYear, CurrentMonth, 0).getDate();
     for (var p = previousMonthLenght - startDay + 1; p <= previousMonthLenght; p++) {
-      let item = { "day": p, "month": CurrentMonth, "reserved": false }
+      let item = { "day": p, "dayName": "", "month": CurrentMonth, "monthName": monthNames[CurrentMonth], "reserved": false }
       prevMonth[p - (previousMonthLenght - startDay + 1)] = item;
 
       // Set Reserved
@@ -59,7 +59,7 @@ export const CalendarModule = (props) => {
     const thisMonthLenght = new Date(currentYear, CurrentMonth + 1, 0).getDate();
     var thisMonth = {};
     for (var t = 0; t <= thisMonthLenght - 1; t++) {
-      let item = { "day": t + 1, "month": CurrentMonth + 1, "reserved": false }
+      let item = { "day": t + 1, "dayName": "", "month": CurrentMonth + 1, "monthName": monthNames[CurrentMonth + 1], "reserved": false }
       thisMonth[t + Object.keys(prevMonth).length] = item;
 
       // Set Reserved
@@ -80,7 +80,7 @@ export const CalendarModule = (props) => {
     var start = 42 - Object.keys(prevMonth).length - Object.keys(thisMonth).length;
     var nextMonth = {};
     for (var n = 41 - start; n < 41; n++) {
-      let item = { "day": start - (41 - n) + 1, "month": CurrentMonth + 2, "reserved": false }
+      let item = { "day": start - (41 - n) + 1, "dayName": "", "month": CurrentMonth + 2, "monthName": monthNames[CurrentMonth + 2], "reserved": false }
       nextMonth[n + 1] = item;
 
       // Set Reserved
@@ -97,28 +97,46 @@ export const CalendarModule = (props) => {
     }
     //console.log(nextMonth);
 
-    let merged = { ...prevMonth, ...thisMonth, ...nextMonth };
-    //console.log(merged);
 
+    // Combine dates from each month
+    let merged = { ...prevMonth, ...thisMonth, ...nextMonth };
+
+    // Find day names 
+    let x = 0;
+    for (let index = 0; index < 42; index++) {
+      merged[index].dayName = dayNames[x];
+      if (x !== 6)
+        x++
+      else
+        x = 0;
+    }
+
+    //console.log(merged);
     return merged;
 
   }, [currentMonth, currentYear]);
+
 
   useEffect(() => {
     setCurrentMonthDays(range(currentMonth));
     setNextMonthDays(range(currentMonth + 1));
   }, [range]);
 
+
   const SelectDate = (index) => {
 
     setClicks(clicks + 1);
 
-    console.log(index);
+    //console.log(index);
 
     var month = (currentMonthDays[index].month === 0) ? (12) : ((currentMonthDays[index].month === 13) ? (1) : (currentMonthDays[index].month));
     var year = (currentMonthDays[index].month === 0) ? (currentYear - 1) : ((currentMonthDays[index].month === 13) ? (currentYear + 1) : (currentYear));
 
     if (selected.bool) {
+
+      if (selected[1] !== null)
+        ChangeState(setSelected, "", "index", 1);
+
       ChangeState(setSelected, index, "index", 0);
       ChangeState(setSelected, currentMonthDays[index].day, "day", 0);
       ChangeState(setSelected, month, "month", 0);
@@ -127,6 +145,7 @@ export const CalendarModule = (props) => {
     }
 
     if (clicks % 2 === 0) {
+
       ChangeState(setSelected, !Boolean(selected.bool), "bool");
       ChangeState(setSelected, index, "index", 1);
       ChangeState(setSelected, currentMonthDays[index].day, "day", 1);
@@ -134,6 +153,10 @@ export const CalendarModule = (props) => {
       ChangeState(setSelected, year, "year", 1);
     }
     else {
+
+      if (selected[1] !== null)
+        ChangeState(setSelected, "", "index", 1);
+
       ChangeState(setSelected, index, "index", 0);
       ChangeState(setSelected, currentMonthDays[index].day, "day", 0);
       ChangeState(setSelected, month, "month", 0);
@@ -141,10 +164,10 @@ export const CalendarModule = (props) => {
     }
   }
 
-  console.log(selected)
+  console.log(selected);
 
   const rows = [];
-  for (let i = 0; i < props.count; i++) {
+  for (let i = 0; i < 1; i++) {
     const calendarDays = (i === 0 ? currentMonthDays : nextMonthDays);
     const calendarMonth = (i === 0 ? currentMonth + 1 : currentMonth + 2);
     rows.push(
