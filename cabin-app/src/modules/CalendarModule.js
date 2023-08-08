3498cf6d97e70ae66e002ccbfce2e772f5e74717
JellonaVesa666 <vesaruntti@gmail.com>
2023-08-09 01:22:04 +0300

    Claendar remove unnecessary async.

--- cabin-app/src/modules/CalendarModule.js
+++ cabin-app/src/modules/CalendarModule.js
@@ -329,7 +329,7 @@ export const CalendarModule = (props) => {
     }
   }
 
-  const ClearSelected = async (forceClear) => {
+  const ClearSelected = (forceClear) => {
 
     if (clicks !== 3 && !forceClear) {
       return false;
