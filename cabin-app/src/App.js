c27f2d973a40f33d2e941d68a868f68e7cd0e2b2
JellonaVesa666 <vesaruntti@gmail.com>
2023-08-09 10:48:23 +0300

    Cabin images.

--- cabin-app/src/App.js
+++ cabin-app/src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
-import { Link, Route, Routes } from "react-router-dom"
+//import { Link, Route, Routes } from "react-router-dom"
 import { BodyBackground } from "./styles/BaseStyle"
 import NavigationModule from "./modules/NavigationModule";
 import { SearchBarModule } from './modules/SearchBarModule';
-import { ContentModule } from "./modules/ContentModule";
+import cabin_01 from "../src/images/cabin_05.jpg";
+import cabin_02 from "../src/images/cabin_08.jpg";
 
 function App() {
 
@@ -13,7 +14,6 @@ function App() {
       <SearchBarModule />
 
       <main>
-        <ContentModule />
         <section class="py-5 text-center container">
           <div class="row py-lg-5">
             <div class="col-lg-6 col-md-8 mx-auto">
@@ -27,157 +27,50 @@ function App() {
           </div>
         </section>
 
-        <div class="album py-5 bg-light">
-          <div class="container">
-
-            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-              <div class="col">
-                <div class="card shadow-sm">
-                  <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-
-                  <div class="card-body">
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <div class="d-flex justify-content-between align-items-center">
-                      <div class="btn-group">
-                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                        <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                      </div>
-                      <small class="text-muted">9 mins</small>
-                    </div>
-                  </div>
+        <div class="row d-flex justify-content-center align-items-center m-0 py-5 bg-light">
+          <div class="row col-3 g-5">
+            <img
+              src={cabin_01}
+              alt="Logo"
+              style={{ objectFit: "cover", width: "100%", height: "300px", }}
+            />
+            <p
+              className="col-6 mt-2"
+              style={{ fontSize: "1.5rem", fontWeight: 400 }}
+            >
+              Ekin Mesta
+            </p>
+            <p className="col-6">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            {/*               <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                  <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
                 </div>
-              </div>
-              <div class="col">
-                <div class="card shadow-sm">
-                  <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+                <small class="text-muted">9 mins</small>
+              </div> */}
 
-                  <div class="card-body">
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <div class="d-flex justify-content-between align-items-center">
-                      <div class="btn-group">
-                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                        <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                      </div>
-                      <small class="text-muted">9 mins</small>
-                    </div>
-                  </div>
+          </div>
+          <div class="row col-3 g-5">
+            <img
+              src={cabin_02}
+              alt="Logo"
+              style={{ objectFit: "cover", width: "100%", height: "300px", }}
+            />
+            <p
+              className="col-6 mt-2"
+              style={{ fontSize: "1.5rem", fontWeight: 400 }}
+            >
+              Ekin Mesta
+            </p>
+            <p className="col-6">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            {/*               <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                  <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
                 </div>
-              </div>
-              <div class="col">
-                <div class="card shadow-sm">
-                  <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+                <small class="text-muted">9 mins</small>
+              </div> */}
 
-                  <div class="card-body">
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <div class="d-flex justify-content-between align-items-center">
-                      <div class="btn-group">
-                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                        <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                      </div>
-                      <small class="text-muted">9 mins</small>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-              <div class="col">
-                <div class="card shadow-sm">
-                  <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-
-                  <div class="card-body">
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <div class="d-flex justify-content-between align-items-center">
-                      <div class="btn-group">
-                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                        <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                      </div>
-                      <small class="text-muted">9 mins</small>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="col">
-                <div class="card shadow-sm">
-                  <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-
-                  <div class="card-body">
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <div class="d-flex justify-content-between align-items-center">
-                      <div class="btn-group">
-                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                        <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                      </div>
-                      <small class="text-muted">9 mins</small>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="col">
-                <div class="card shadow-sm">
-                  <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-
-                  <div class="card-body">
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <div class="d-flex justify-content-between align-items-center">
-                      <div class="btn-group">
-                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                        <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                      </div>
-                      <small class="text-muted">9 mins</small>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-              <div class="col">
-                <div class="card shadow-sm">
-                  <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-
-                  <div class="card-body">
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <div class="d-flex justify-content-between align-items-center">
-                      <div class="btn-group">
-                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                        <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                      </div>
-                      <small class="text-muted">9 mins</small>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="col">
-                <div class="card shadow-sm">
-                  <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-
-                  <div class="card-body">
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <div class="d-flex justify-content-between align-items-center">
-                      <div class="btn-group">
-                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                        <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                      </div>
-                      <small class="text-muted">9 mins</small>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="col">
-                <div class="card shadow-sm">
-                  <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-
-                  <div class="card-body">
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <div class="d-flex justify-content-between align-items-center">
-                      <div class="btn-group">
-                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                        <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                      </div>
-                      <small class="text-muted">9 mins</small>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
           </div>
         </div>
 
--- cabin-app/src/mockup/cabinData.js
+++ cabin-app/src/mockup/cabinData.js
@@ -13,7 +13,7 @@ export const cabinData = {
     discountFromDays: 5,
     discountExpriration: "2023-07-04T13:33:03.969Z",
     discountMultiplier: 150,
-    name: "Mökki 01",
+    name: "Ekin Mesta",
     location: "Etelä-Suomi / Vihti, Uusimaa",
     features: `Pinnasänky(Pyynnöstä), Astianpesukone, Pakastin, Syöttötuoli, Sauna, BBQ-Grilli, Soutuvene
     , Kahvinkeitin, Takka, Jääkaappi, Mikroaaltouuni, TV, Yksityinen ranta`,
@@ -43,3 +43,36 @@ export const cabinData = {
     }
   }
 }
+
+/* [
+  {
+      "id": 1,
+      "ownerId": 8,
+      "name": "Ekin Mesta",
+      "region": "Pohjois-Pohjanmaa",
+      "address": "HattiWattikuja 1",
+      "postalCode": "90500",
+      "mapCoordinates": "x:200, y:500",
+      "isActive": 1,
+      "status": 0,
+      "capacity": "2",
+      "floors": 1,
+      "bedrooms": 1,
+      "bathrooms": 1,
+      "squares": 30,
+      "plotSize": 0,
+      "type": 0,
+      "features": "",
+      "description": "Mökin tilavat makuuhuoneet on sisustettu rauhallisesti ja sopusoinnussa hirsiseinien kanssa.Täällä nukut hyvät yöunet ja heräät virkeänä aamuun. Hirsimökissä on upea sauna, jonka järeät lauteet on tehty tervaleppä lankusta ja kokonaisuuden kruunaa Veto -kiuas.Hirsimökissä on tilava katettu terassi, jolta voi ihailla järveä. Terassilla nautit aamukahvit tai vaikka illallisen ympäri vuoden luonnosta nauttien. Mökin omasta rannasta pääset rauhassa uimaan tai soutelemaan järvelle. Villa Einon nuotiopaikka on aivan järven rannalla. Siinä keität mukavasti nokipannukahvit, paistat makkarat tai vaikka muurinpohjalettuja. Nuotiopaikalle mahtuu kahdeksan henkeä.",
+      "arrivalTime": "16:00:00",
+      "departureTime": "12:00:00",
+      "price": 230,
+      "discount": 0,
+      "discountPercent": 0,
+      "discountExpriration": "2019-01-06T17:16:40",
+      "discountMultiplier": 0,
+      "discountFromDays": 0,
+      "createdDate": "2019-01-06T17:16:40",
+      "modifiedDate": "2019-01-06T17:16:40"
+  }
+] */
